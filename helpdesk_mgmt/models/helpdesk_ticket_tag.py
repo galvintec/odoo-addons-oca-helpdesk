brain-tec/helpdesk

[IMP] helpdesk_mgmt: Sequence + translatable in ticket tags
- Add a sequence and allow to sort them.
- Make tag name translatable.

TT57090

helpdesk_mgmt/models/helpdesk_ticket_tag.py
@@ -4,8 +4,10 @@ from odoo import fields, models
 class HelpdeskTicketTag(models.Model):
     _name = "helpdesk.ticket.tag"
     _description = "Helpdesk Ticket Tag"
+    _order = "sequence,id"
 
-    name = fields.Char()
+    sequence = fields.Integer(default=10)
+    name = fields.Char(translatable=True)
     color = fields.Integer(string="Color Index")
     active = fields.Boolean(default=True)
     company_id = fields.Many2one(

galvintec/odoo-addons-oca-helpdesk

helpdesk_mgmt/models/helpdesk_ticket_tag.py
@@ -4,8 +4,10 @@ from odoo import fields, models
 class HelpdeskTicketTag(models.Model):
     _name = "helpdesk.ticket.tag"
     _description = "Helpdesk Ticket Tag"
+    _order = "sequence,id"
 
-    name = fields.Char()
+    sequence = fields.Integer(default=10)
+    name = fields.Char(translate=True)
     color = fields.Integer(string="Color Index")
     active = fields.Boolean(default=True)
     company_id = fields.Many2one(