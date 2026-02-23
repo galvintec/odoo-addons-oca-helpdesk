e0620f796fe62303728854c7ee197252d9310854
Gabriel Nogueira <gabi@galvintec.com>
2026-02-23 10:24:01 +0100

    [MIG] helpdesk_mgmt: Migration to 19.0

--- helpdesk_mgmt/__manifest__.py
+++ helpdesk_mgmt/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Helpdesk Management",
     "summary": """
         Helpdesk""",
-    "version": "18.0.1.16.8",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "category": "After-Sales",
     "author": "AdaptiveCity, "
